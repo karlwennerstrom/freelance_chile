b757b21cd98fc5925f01d2fecc550e937345e9f6
Karl <karlwennerstrom@gmail.com>
2022-07-05 20:38:33 -0400

    Testing node server 2

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const { signed } = require('xpress/lib/string');
 var app = express();
 app.use(express.json());
 
-var conexion= mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'gestion_boletas'
-})
+// var conexion= mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     database:'gestion_boletas'
+// })
 
 
 //Testing Conection
@@ -29,56 +29,56 @@ app.get('/',function(req,res){
     res.send('Ruta Inicio');
 })
 
-//obtener todas las boletas
+// //obtener todas las boletas
 
-app.get('/api/boletas',(req,res)=>{
+// app.get('/api/boletas',(req,res)=>{
 
-    conexion.query('SELECT * FROM boletas', (error,filas)=>{
+//     conexion.query('SELECT * FROM boletas', (error,filas)=>{
 
-        if(error){
-            throw error;
-        }else{
-            res.send(filas);
-        }
+//         if(error){
+//             throw error;
+//         }else{
+//             res.send(filas);
+//         }
 
-    })
+//     })
 
-})
-process.on('uncaughtException', function(err) {
-    console.log(err);
-  });
-//crear boleta
+// })
+// process.on('uncaughtException', function(err) {
+//     console.log(err);
+//   });
+// //crear boleta
 
-app.post('/api/boletas',(req,res)=>{
-    let data = {
-        titulo:req.body.titulo,
-        descripcion:req.body.descripcion,
-        valor:req.body.valor
-    };
-    let sql="INSERT INTO boletas SET ?";
-    conexion.query(sql,data,function(error,results){
-        if(error){
-            throw error;
-        }else{
-            res.send(results);
-        }
-    })
+// app.post('/api/boletas',(req,res)=>{
+//     let data = {
+//         titulo:req.body.titulo,
+//         descripcion:req.body.descripcion,
+//         valor:req.body.valor
+//     };
+//     let sql="INSERT INTO boletas SET ?";
+//     conexion.query(sql,data,function(error,results){
+//         if(error){
+//             throw error;
+//         }else{
+//             res.send(results);
+//         }
+//     })
 
-});
+// });
 
-app.get('/api/boletas/:id',(req,res)=>{
+// app.get('/api/boletas/:id',(req,res)=>{
 
-    conexion.query('SELECT * FROM boletas WHERE id=?',[req.params.id], (error,fila)=>{
+//     conexion.query('SELECT * FROM boletas WHERE id=?',[req.params.id], (error,fila)=>{
 
-        if(error){
-            throw error;
-        }else{
-            res.send(fila);
-        }
+//         if(error){
+//             throw error;
+//         }else{
+//             res.send(fila);
+//         }
 
-    })
+//     })
 
-})
+// })
 
 const puerto = 3000;
 app.listen(process.env.PORT || puerto,function(){
