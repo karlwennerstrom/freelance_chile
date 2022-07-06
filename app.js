9b9b7f4581f06ffc0e7aa391bae15beb07043249
Karl <karlwennerstrom@gmail.com>
2022-07-06 12:09:26 -0400

    Added HandleDisconnect, for mange disconnect problems

--- app.js
+++ app.js
@@ -5,24 +5,47 @@ const { signed } = require('xpress/lib/string');
 var app = express();
 app.use(express.json());
 
-var conexion= mysql.createConnection({
-    host:'us-cdbr-east-06.cleardb.net',
-    user:'b39bb4d3711279',
-    password:'e4124551',
-    database:'heroku_e127056c9502aab'
-})
+
 
 
 //Testing Conection
-conexion.connect(function(error){
-    if(error){
-        throw error;
-    }
-    else{
-        console.log("Conexión exitosa");
-    }
-})
+// conexion.connect(function(error){
+//     if(error){
+//         throw error;
+//     }
+//     else{
+//         console.log("Conexión exitosa");
+//     }
+// })
 
+
+function handleDisconnect() {
+
+    var conexion= mysql.createConnection({
+        host:'us-cdbr-east-06.cleardb.net',
+        user:'b39bb4d3711279',
+        password:'e4124551',
+        database:'heroku_e127056c9502aab'
+    })
+
+  conexion.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.get('/',function(req,res){
     res.send('Ruta Inicio');
